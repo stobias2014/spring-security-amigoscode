c1360646d4b9410353b914d6df0d56c66e2b9867
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-19 14:15:32 -0500

    added spring security project

---
+++ src/test/java/com/tobias/saul/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
